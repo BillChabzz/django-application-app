ed69f18196d92546de274881e7cf1e5be878a15d
BillChabzz <chaby9.8@gmail.com>
2015-10-06 10:11:00 +0300

    added forms to html and saving functionality

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import StudentForm
 # Create your views here.
 def index(request):
+    form = StudentForm(request.POST or None)
     context = {
-        "hello_message": "Hello Moringa"
+        "hello_message": "Register new student",
+        "form" : form
     }
+    
+    if form.is_valid():
+        form.save()
+        context = {
+            "hello_message": "Student saved!"
+        }
+    print (request.POST)
     return render(request, "index.html", context)
     
